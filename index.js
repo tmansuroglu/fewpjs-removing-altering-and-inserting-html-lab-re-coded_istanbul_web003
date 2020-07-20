75e982b026f5b7ed1a4e208ac99563a6ae47384d
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-07-20 06:29:30 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.querySelector("main#main").remove();
+const a = document.createElement("h1");
+document.body.appendChild(a);
+a.id = "victory";
+const newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = "YOUR-NAME is the champion"
